ca2ff34e8b68c416870497f65eafa7b18b4e2ceb
Extreme <najarvis05@gmail.com>
2021-08-17 21:53:08 -0500

    Additional Handlers + activate them.

--- src/bot/VoteTracker.ts
+++ src/bot/VoteTracker.ts
@@ -1,10 +1,13 @@
-import { Client, ClientOptions } from 'discord.js'
+import { Client, ClientOptions, Collection } from 'discord.js'
 import { readdirSync } from 'fs'
+import { isAbsolute, resolve } from 'path';
 import { Database } from '../database';
 import Master from '../structures/Master';
+import { CommandContext } from './CommandContext';
 export default class VoteTracker extends Client {
-  master: Master;
+  master: Master
   db: Database
+  commands: Collection<string, CommandContext> = new Collection()
   constructor(options: ClientOptions, master: Master) {
     super(options);
     this.master = master
@@ -12,6 +15,11 @@ export default class VoteTracker extends Client {
   }
 
   construct(): void {
+    this.loadEvents('./events')
+    this.loadCommands('./commands')
+    this.on('ready', () => {
+      this.user?.setActivity('Votes go brrrrrr')
+    })
     this.login('')
   }
 
@@ -33,6 +41,25 @@ export default class VoteTracker extends Client {
       this.on(file.name.slice(0, -3) as any, event.bind(null, this))
 
       delete require.cache[require.resolve(filePath)]
+    })
+  }
+  public loadCommands(dir: string): void {
+    if (!isAbsolute(dir)) dir = resolve(process.cwd(), dir)
+
+    const files = readdirSync(dir, { withFileTypes: true })
+
+    files.forEach(file => {
+      if (file.name.endsWith('.js')) return
+
+      delete require.cache[require.resolve(resolve(dir, file.name))]
+
+      let command = require(resolve(dir, file.name))
+
+      if (!command) return
+
+      if (command.default) command = command.default
+
+      this.commands.set(command.data.name, command)
     })
   }
 }
