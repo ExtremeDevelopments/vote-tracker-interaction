6c029241909e10c100ae6d6072b56168e8c0051f
Extreme <najarvis05@gmail.com>
2021-08-17 21:35:51 -0500

    ctx updates - Update dependencies to include discordjs/builders

--- src/bot/CommandContext.d.ts
+++ src/bot/CommandContext.d.ts
@@ -1,10 +1,10 @@
 import { ApplicationCommandOptionData, Interaction } from 'discord.js'
 import VoteTracker from './VoteTracker'
-
+import { SlashCommandBuilder } from '@discordjs/builders'
 export interface CommandContext {
-  name: string
-  description: string
-  options: ApplicationCommandOptionData[]
+  data: SlashCommandBuilder
+  options: Array<ApplicationCommandOptionData>
   defaultPermission: boolean
+  data: SlashCommandBuilder
   exec: (client: VoteTracker, interaction: Interaction) => any
 }
