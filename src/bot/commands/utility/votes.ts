5f4e57dcacf1b4352741b4ed4c2ccce5d1fdb883
Extreme <najarvis05@gmail.com>
2021-09-11 15:12:00 -0500

    Add votes command

---
+++ src/bot/commands/utility/votes.ts
@@ -0,0 +1,39 @@
+import { ApplicationCommandOptionType } from "discord-api-types";
+import { CommandOptions } from "discord-rose";
+import { colors } from "../../../structures/extensions/Colors";
+
+export default <CommandOptions>{
+  command: 'votes',
+  interaction: {
+    name: 'votes',
+    description: 'Add, remove, or check votes of a user!',
+    options: [
+      {
+        name: 'user',
+        description: 'User to check for.',
+        type: ApplicationCommandOptionType.User,
+        required: true
+      },
+      {
+        name: 'ephermal',
+        description: 'Whether or not to make the message ephermal.',
+        type: ApplicationCommandOptionType.Boolean
+      }
+    ]
+  },
+  exec: async (ctx) => {
+    if (!ctx.guild) return
+    if (ctx.isInteraction) ctx.args = [ctx.options]
+    const user = ctx.options.user || ctx.author.id
+    const userData = await ctx.worker.db.users.getUserVotes(user, ctx.guild.id)
+    const foundUser = ctx.worker.users.get(user)
+      ?? await ctx.worker.api.users.get(user).catch(e => null)
+    const embed = ctx.embed
+      .title(`${foundUser ? foundUser.username : `User`}'s vote information.`)
+      .description(`**Total votes:** ${userData.total_votes}
+      **Last vote:** ${userData.last_vote === null ? `Never` : userData.last_vote.toLocaleTimeString()}
+      `)
+      .color(colors.ORANGE)
+    ctx.reply({ embeds: [embed.render()] }, false, ctx.options.ephermal || false)
+  }
+}
