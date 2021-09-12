45cc1397c1faeeef6c8ccb38cdc60434fbbde5ed
Extreme <najarvis05@gmail.com>
2021-09-11 21:05:31 -0500

    Add InfluxDB handling

--- src/structures/client/VTWorker.ts
+++ src/structures/client/VTWorker.ts
@@ -4,6 +4,7 @@ import { readdirSync } from "fs";
 import fetch from "node-fetch";
 import { Database } from "../database";
 import { WorkerAPI } from "../API/WorkerAPI";
+import InfluxHandler from "../handlers/InfluxHandler";
 export class VTWorker extends SingleWorker {
   handlers = {
     interactions: null,
@@ -11,6 +12,7 @@ export class VTWorker extends SingleWorker {
   }
   db = new Database('mongodb://localhost:27017/votetracker')
   rest = new WorkerAPI(this)
+  influx = new InfluxHandler(this)
   public loadMiddlewares(dir: string): any {
     const files = readdirSync(dir, { withFileTypes: true })
     for (const file of files) {
@@ -28,5 +30,11 @@ export class VTWorker extends SingleWorker {
 
       delete require.cache[require.resolve(filePath)]
     }
+  }
+  get mem(): NodeJS.MemoryUsage {
+    return Object.entries(process.memoryUsage()).reduce<any>((T, [K, V]) => {
+      T[K] = (V / 1024 ** 2).toFixed(1) + 'MB'
+      return T
+    }, {})
   }
 }
---
+++ src/structures/handlers/InfluxHandler.ts
@@ -0,0 +1,82 @@
+import { VTWorker } from "../../structures/client/VTWorker"
+import { influx as config } from '../../config.json'
+import { InfluxDB, Point } from "@influxdata/influxdb-client"
+
+export default class InfluxHandler {
+  client = new InfluxDB({
+    url: config.url,
+    token: config.token
+  })
+  eventCounter: Array<Counter> = []
+  dbCounter: Array<Counter> = []
+  commandCounter: Array<Counter> = []
+  constructor(private readonly worker: VTWorker) {
+    this.worker.on('*', data => {
+      this.handleEvent(data)
+    })
+  }
+  sendStats(name: string, count: number, type: string): void {
+    const writeAPI = this.client.getWriteApi(config.org, config.bucket)
+      .useDefaultTags({ host: name })
+    try {
+      writeAPI.writePoint(new Point(type).intField("count", count))
+      writeAPI.close()
+    } catch (e) { }
+  }
+  sendBaseStats(): void {
+    const writeAPI = this.client.getWriteApi(config.org, config.bucket)
+      .useDefaultTags({ host: `worker` })
+    try {
+      writeAPI.writePoints([
+        this.getMemoryStats(),
+        new Point('servers').intField('count', this.worker.guilds.size),
+        new Point('ping').intField('count', this.worker.shards.first()?.ping)
+      ])
+      writeAPI.close()
+    } catch (e) { }
+  }
+  getMemoryStats(): Point {
+    return new Point('memory')
+      .intField('rss', this.worker.mem.rss)
+      .intField('heap-used', this.worker.mem.heapUsed)
+      .intField('heap-total', this.worker.mem.heapTotal)
+      .intField('array-buffers', this.worker.mem.arrayBuffers)
+      .intField('external', this.worker.mem.external)
+  }
+  startTimers(): void {
+    setInterval(() => {
+      this.eventCounter.forEach(e => {
+        this.sendStats(e.name, e.count, 'events')
+        e.count = 0
+      })
+      this.dbCounter.forEach(e => {
+        this.sendStats(e.name, e.count, 'database')
+        e.count = 0
+      })
+      this.commandCounter.forEach(e => {
+        this.sendStats(e.name, e.count, 'commands')
+        e.count = 0
+      })
+    }, 5000)
+  }
+  private handleEvent(data: any): void {
+    if (data.t === null) return
+    const found = this.eventCounter.find(e => e.name === data.t)
+    if (!found) this.eventCounter.push({ name: data.t, count: 1 })
+    if (found) found.count++
+  }
+  addDBCount(name: string) {
+    const found = this.dbCounter.find(e => e.name === name)
+    if (!found) this.dbCounter.push({ name, count: 1 })
+    if (found) found.count++
+  }
+  commandRan(name: string) {
+    const found = this.commandCounter.find(e => e.name === name)
+    if (!found) this.commandCounter.push({ name, count: 1 })
+    if (found) found.count++
+  }
+}
+export interface Counter {
+  name: string
+  count: number
+}
