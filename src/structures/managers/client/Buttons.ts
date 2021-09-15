6004749549e4a3f41cf4a8c48d0647d8a8528f72
Extreme <najarvis05@gmail.com>
2021-09-14 19:04:28 -0500

    Complete button interactions

--- src/structures/managers/client/Buttons.ts
+++ src/structures/managers/client/Buttons.ts
@@ -1,19 +1,17 @@
 import { Cache } from "@jpbberry/cache";
 import { APIMessageComponentInteraction, ComponentType, GatewayInteractionCreateDispatchData, InteractionType } from "discord-api-types";
+import fetch from "node-fetch";
 import { VTWorker } from "../../client/VTWorker";
-/**
- * WIP
- * 1. Add response when incorrect user clicks trash can
- */
 export class ButtonManager {
   worker: VTWorker;
   cache = new Cache<string, ButtonDoc>(15 * 60 * 1000)
   constructor(worker: VTWorker) {
     this.worker = worker
     this.worker.on('INTERACTION_CREATE', (data) => {
+
       if (data.type !== InteractionType.MessageComponent) return
       if (data.data.component_type !== ComponentType.Button) return
-      if (data.message.author.id !== this.worker.user.id) return
+      if (data.message.author.id !== this.worker.user.id) return this.sendInteraction(data, 'You must be the author of the command to use this interaction!')
       this.handleButton(data)
     })
   }
@@ -28,8 +26,21 @@ export class ButtonManager {
   createButton(ID: string, data: ButtonDoc) {
     this.cache.set(ID, data)
   }
-  sendInteraction(data: GatewayInteractionCreateDispatchData) {
-
+  sendInteraction(data: GatewayInteractionCreateDispatchData, content?: string) {
+    fetch(`https://discord.com/api/v9/interactions/${data.id}/${data.token}/callback`, {
+      method: 'POST',
+      body: JSON.stringify({
+        data: {
+          content,
+          flags: 1 << 6,
+        },
+        type: 4
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bot ${this.worker.options.token}`
+      }
+    }).catch(e => null)
   }
 }
 export interface ButtonDoc {
