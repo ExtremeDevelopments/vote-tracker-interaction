cd78480e9d8a197e7365e0af3836355ed09af6ef
Extreme <najarvis05@gmail.com>
2021-09-25 16:26:59 -0500

    Add middleware responses

--- src/bot/middlewares/guildOnly.ts
+++ src/bot/middlewares/guildOnly.ts
@@ -1,7 +1,10 @@
 import { CommandContext } from "discord-rose";
 export default () => {
   return async (ctx: CommandContext) => {
-    if (!ctx.guild && ctx.command.guildOnly) return false
+    if (!ctx.guild && ctx.command.guildOnly) {
+      ctx.reply(`This command can only be ran within a guild!`)
+      return false
+    }
     
     return true
   }
--- src/bot/middlewares/onlyInteraction.ts
+++ src/bot/middlewares/onlyInteraction.ts
@@ -3,7 +3,10 @@ import { CommandContext } from "discord-rose"
 export default () => {
   return async (ctx: CommandContext) => {
 
-    if (ctx.command.interactionOnly && !ctx.isInteraction) return false
+    if (ctx.command.interactionOnly && !ctx.isInteraction) {
+      ctx.reply(`This command is only usable via slash commands!`)
+      return false
+    }
 
     return true
   }
