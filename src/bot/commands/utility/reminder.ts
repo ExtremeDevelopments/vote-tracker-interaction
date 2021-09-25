c7939315f5b7baa09a19103da1542ad13089a20e
Extreme <najarvis05@gmail.com>
2021-09-25 15:17:01 -0500

    Fix colors

--- src/bot/commands/utility/reminder.ts
+++ src/bot/commands/utility/reminder.ts
@@ -1,7 +1,8 @@
 import { ApplicationCommandOptionType } from "discord-api-types";
 import { CommandOptions } from "discord-rose";
+import { colors } from "../../../structures/extensions/Colors";
 
-export default <CommandOptions> {
+export default <CommandOptions>{
   command: 'reminders',
   interaction: {
     name: 'reminders',
@@ -23,15 +24,15 @@ export default <CommandOptions> {
     if (!ctx.guild) return
     if (ctx.isInteraction) ctx.args = [ctx.options]
     const user = await ctx.worker.db.users.getUser(ctx.author.id)
-    if(ctx.options.enable) {
+    if (ctx.options.enable) {
       user.reminders = true
       await ctx.worker.db.users.updateUser(user)
-      ctx.reply({ embeds: [{ description: 'Reminder option enabled!'}]})
+      ctx.reply({ embeds: [{ description: 'Reminder option enabled!', color: colors.ORANGE }] }, false, true)
     }
-    if(ctx.options.disable) {
+    if (ctx.options.disable) {
       user.reminders = false
       await ctx.worker.db.users.updateUser(user)
-      ctx.reply({ embeds: [{ description: 'Reminder option disabled!'}]})
+      ctx.reply({ embeds: [{ description: 'Reminder option disabled!', color: colors.ORANGE }] }, false, true)
     }
   }
 }
